7679d0c39ea4bcb2624df7f88291c888da5fa2c1
lovishbansal-sde <81516480+lovishbansal-sde@users.noreply.github.com>
2022-04-15 02:14:36 +0530

    Add files via upload

---
+++ inter.cpp
@@ -0,0 +1,55 @@
+fun(node *head)
+{
+    if(head->next==NULL) return ;
+    fun(head->next);
+    cout<<head->data<<endl;
+    }
+1 -- 2 -- 3 -- 4 -- 5 -- 6 -- 7 -- 8 -- 9 
+9 8 7 -----1
+fn(vector<string> s)
+{
+    int i=0;
+    int j=s.size();
+    while(i<=j)
+    {
+        swap(s[i],s[j]);
+        i++;
+        j--;
+    }
+}
+str=My name is lovish 
+0 1 2
+int p=0;
+for(int i=0;i<str.length();i++)
+{
+    
+
+    if(str[i]==' ')
+    {
+        strr=str.substr(p,i); //My
+        s.push_back(strr);
+        p=i; p==2
+    }
+    
+}
+vector{'My','Name','is','Loviuh'}
+1    2
+
+count=0;
+while(n>0)
+{
+    ans=n&1
+    if(ans==1){ count++;}
+    n>>1;
+}
+
+1 1 0 0 1 0 1 
+0 1 1 0 0 1 0//4
+
+0 0 0 0 0 0 1
+
+
+
+
+
+1 0 1 
